a8c67f2797dd076a38d46409085ed41ea4babd34
Zuzanna Wójcik <zuzwoj14@st.amu.edu.pl>
2024-02-07 19:25:27 +0100

    zmiany w spritach

--- cw 7/src/project.hpp
+++ cw 7/src/project.hpp
@@ -5,7 +5,6 @@
 #include <iostream>
 #include <cmath>
 
-
 #include "Shader_Loader.h"
 #include "Render_Utils.h"
 #include "Render_Sprite.h"
@@ -54,7 +53,6 @@ GLuint programSun;
 GLuint programSprite;
 GLuint programSkybox;
 GLuint skyboxTexture;
-
 GLuint programBlur;
 GLuint programBloomFinal;
 
@@ -72,11 +70,6 @@ glm::vec3 spotlightPos = glm::vec3(0, 0, 0);
 glm::vec3 spotlightConeDir = glm::vec3(0, 0, 0);
 glm::vec3 spotlightColor = glm::vec3(0.4, 0.4, 0.9) * 3;
 float spotlightPhi = 3.14 / 4;
-
-bool showMissions = false;
-bool hideInstruction = false;
-bool missionsComplete = false;
-bool raceCompleted = false;
 
 float aspectRatio = 1.f;
 float exposition = 1.f;
@@ -96,14 +89,16 @@ float deltaTime = 0.f;
 
 float spaceshipRadius = 0.5f;
 int trashDestroyed = 0;
+bool showMissions = false;
+bool hideInstruction = false;
+bool raceCompleted = false;
+bool trashCompleted = false;
+bool hideCompleted = true;
 int circleVisited = 0;
-
-unsigned int bloomTexture;
 
 std::chrono::time_point<std::chrono::steady_clock> start_time;
 std::chrono::time_point<std::chrono::steady_clock> end_time;
 
-bool bloom = true;
 float exposure = 0.8f;
 unsigned int hdrFBO;
 unsigned int pingpongFBO[2];
@@ -128,13 +123,11 @@ void renderQuad()
 	if (quadVAO == 0)
 	{
 		float quadVertices[] = {
-			// positions        // texture Coords
 			-1.0f,  1.0f, 0.0f, 0.0f, 1.0f,
 			-1.0f, -1.0f, 0.0f, 0.0f, 0.0f,
 			 1.0f,  1.0f, 0.0f, 1.0f, 1.0f,
 			 1.0f, -1.0f, 0.0f, 1.0f, 0.0f,
 		};
-		// setup plane VAO
 		glGenVertexArrays(1, &quadVAO);
 		glGenBuffers(1, &quadVBO);
 		glBindVertexArray(quadVAO);
@@ -215,8 +208,6 @@ void drawTrash(float planetX, float planetZ, float time, float orbitRadius, glm:
 	}
 }
 
-
-
 void drawPlanet(Core::RenderContext& context, TextureSet textures, float planetOrbitRadius, float planetOrbitSpeed, float time, glm::vec3 scalePlanet, float trashOrbitRadius, std::string& planetName) {
 	float planetX = planetOrbitRadius * cos(planetOrbitSpeed * time);
 	float planetZ = planetOrbitRadius * sin(planetOrbitSpeed * time);
@@ -244,7 +235,6 @@ void drawPlanet(Core::RenderContext& context, TextureSet textures, float planetO
 	Core::SetActiveTexture(textures.metallic, "metallicTexture", programDefault, 4);
 	Core::DrawContext(context);
 
-	// THRASH
 	drawTrash(planetX, planetZ, time, trashOrbitRadius,scalePlanet,planetName);
 }
 
@@ -302,6 +292,7 @@ bool checkCollision(glm::vec3 object1Pos, float object1Radius) {
 				std::chrono::duration<double> elapsed_seconds = end_time - start_time;
 				if (elapsed_seconds.count() < 15.0) {
 					raceCompleted = true;
+					renderSprite->UpdateSprite(sprites.sprite_3);
 					std::cout << "skonczyles wyscig";
 				}
 				else
@@ -333,12 +324,10 @@ void renderScene(GLFWwindow* window)
 	Core::DrawSkybox(programSkybox, contexts.skyboxContext, skyboxTexture, cameraDir, cameraPos, aspectRatio);
 	glClear(GL_DEPTH_BUFFER_BIT);
 
-	// S£OÑCE
 	glm::vec3 sunPosition = glm::vec3(0, 0, 0);
 	planets.planetsProperties["Sun"] = { sunPosition, 30.f };
 	drawSun(contexts.sphereContext, glm::scale(glm::vec3(30.f)) * glm::translate(sunPosition), textures.sun);
 
-	// UK£AD S£ONECZNY - PLANETY (NA RAZIE BEZ KSIÊ¯YCA)
 	drawPlanet(contexts.sphereContext, textures.planets.mercury, 15.0f * 5, 0.2f, time, glm::vec3(0.5 * 9), 1 * 9, std::string("Mercury"));
 	drawPlanet(contexts.sphereContext, textures.planets.venus, 20.0f * 5, 0.175f, time, glm::vec3(1.f * 9), 1.5 * 9, std::string("Venus"));
 	drawPlanet(contexts.sphereContext, textures.planets.earth, 25.0f * 5, 0.15f, time, glm::vec3(1.3f * 9), 2 * 9, std::string("Earth"));
@@ -348,14 +337,12 @@ void renderScene(GLFWwindow* window)
 	drawPlanet(contexts.sphereContext, textures.planets.uran, 55.0f * 5, 0.05f, time, glm::vec3(1.6f * 9), 2.5 * 9, std::string("Uran"));
 	drawPlanet(contexts.sphereContext, textures.planets.neptune, 60.0f * 5, 0.025f, time, glm::vec3(1.8f * 9), 2.5 * 9, std::string("Neptun"));
 
-	//LICZBY LOSOWE
 	glm::vec3 initialAsteroidPosition(0.f, 40.f, 0.f);
 	float offset = sin(time) * 2.0f;
 
 	std::default_random_engine generator; // Inicjalizacja generatora liczb losowych
 	std::uniform_real_distribution<float> distribution(-1.0f, 1.0f); // Zakres losowych wartoœci od -1.0 do 1.0
 
-	//asteroidy
 	for (int row = 0; row < 3; ++row)
 	{
 		for (int col = 0; col < 8; ++col)
@@ -364,7 +351,6 @@ void renderScene(GLFWwindow* window)
 			if (row % 2 == 1) position.x += 10.f * 0.5f;
 			position.x += offset;
 
-			// Dodaj losowoœæ do pozycji asteroidy
 			position.x += distribution(generator) * 5.f;
 			position.y += distribution(generator) * 5.f;
 			position.z += distribution(generator) * 5.f;
@@ -405,7 +391,6 @@ void renderScene(GLFWwindow* window)
 	drawObjectTexture(programDefault, contexts.asteroidContext, textures.asteroid, transformation);
 	asteroidPositions[3][3] = position;
 
-	// tor wyœcigowy
 	transformation = glm::translate(glm::vec3(0.f, -50.f, 0.f))*glm::scale( glm::vec3(50.f))* glm::rotate(glm::radians(270.f), glm::vec3(1.0f, 0.f, 0.0f));
 	drawObjectTexture(programDefault, contexts.barierContext, textures.barier, transformation);
 	transformation = glm::translate(glm::vec3(0.f, -50.f, 0.f)) * glm::scale(glm::vec3(70.f)) * glm::rotate(glm::radians(270.f), glm::vec3(1.0f, 0.f, 0.0f));
@@ -414,27 +399,23 @@ void renderScene(GLFWwindow* window)
 	TextureSet texture;
 	bool visited;
 
-	// Rysuj pierwsze cztery okrêgi
 	auto it = circlePositions.begin();
 	for (int i = 0; i < 4; ++i, ++it) {
 		const glm::vec3& pos = it->second.first;
 		bool visited = it->second.second;
 		if (visited) texture = textures.circle_dark; else texture = textures.circle_bright;
-		// Rysuj okr¹g z odpowiedni¹ tekstur¹ i obrótem
 		drawObjectTexture(programDefault, contexts.circleContext, texture,
 			glm::translate(pos) * glm::scale(glm::vec3(15.f)) * glm::rotate(glm::radians(270.f), glm::vec3(1.0f, 0.f, 0.0f)));
 	}
 
-	// Rysuj kolejne cztery okrêgi
 	for (; it != circlePositions.end(); ++it) {
 		const glm::vec3& pos = it->second.first;
 		bool visited = it->second.second;
 		if (visited) texture = textures.circle_dark; else texture = textures.circle_bright;
-		// Rysuj okr¹g z odpowiedni¹ tekstur¹ i innym obrótem
 		drawObjectTexture(programDefault, contexts.circleContext, texture,
 			glm::translate(pos) * glm::scale(glm::vec3(15.f)) * glm::rotate(glm::radians(270.f), glm::vec3(1.0f, 0.f, 0.0f)) * glm::rotate(glm::radians(90.f), glm::vec3(0.f, 0.f, 1.0f)));
 	}
-	//STATEK
+
 	glm::vec3 spaceshipSide = glm::normalize(glm::cross(spaceshipDir, glm::vec3(0.f, 1.f, 0.f)));
 	glm::vec3 spaceshipUp = glm::normalize(glm::cross(spaceshipSide, spaceshipDir));
 	glm::mat4 spaceshipCameraRotationMatrix = glm::mat4({
@@ -445,7 +426,6 @@ void renderScene(GLFWwindow* window)
 		});
 	drawObjectTexture(programDefault, contexts.shipContext, textures.spaceship, glm::translate(spaceshipPos) * spaceshipCameraRotationMatrix * glm::eulerAngleY(glm::pi<float>()) * glm::scale(glm::vec3(0.0004)));
 
-	// Czy wystrzelono laser
 	if (laser.isActive)
 	{
 		float currentTime = glfwGetTime();
@@ -453,10 +433,9 @@ void renderScene(GLFWwindow* window)
 
 		if (elapsedTime < laser.duration)
 		{
-			float laserSpeed = 50.f;
+			float laserSpeed = 150.f;
 			laser.position += laser.direction * laserSpeed * deltaTime;
 
-			// Rysuj laser
 			glm::mat4 laserModelMatrix = glm::translate(laser.position) * glm::scale(glm::vec3(0.0003));
 			drawObjectTexture(programDefault, contexts.laserContext, textures.laser, laserModelMatrix);
 			if(checkCollision(laser.position,0.5f)) laser.isActive = false;
@@ -477,12 +456,14 @@ void renderScene(GLFWwindow* window)
 	if (trashDestroyed == 10)
 	{
 		renderSprite->UpdateSprite(sprites.sprite_2);
-		missionsComplete = true;
+		trashCompleted = true;
 	}
-	if (missionsComplete)
+	if (trashCompleted && raceCompleted)
 	{
-		renderSpriteEnd->DrawSprite(programSprite, 740.0f, 580.0f);
+		renderSprite->UpdateSprite(sprites.sprite_4);
+		hideCompleted = false;
 	}
+	if (!hideCompleted) renderSpriteEnd->DrawSprite(programSprite, 740.0f, 580.0f);
 
 	glBindFramebuffer(GL_FRAMEBUFFER, 0);
 
@@ -494,7 +475,7 @@ void renderScene(GLFWwindow* window)
 		glBindFramebuffer(GL_FRAMEBUFFER, pingpongFBO[horizontal]);
 		glUniform1i(glGetUniformLocation(programBlur, "horizontal"), horizontal);
 		glActiveTexture(GL_TEXTURE0);
-		glBindTexture(GL_TEXTURE_2D, first_iteration ? colorBuffers[1] : pingpongColorbuffers[!horizontal]);  // bind texture of other framebuffer (or scene if first iteration)
+		glBindTexture(GL_TEXTURE_2D, first_iteration ? colorBuffers[1] : pingpongColorbuffers[!horizontal]);
 		renderQuad();
 		horizontal = !horizontal;
 		if (first_iteration)
@@ -508,7 +489,7 @@ void renderScene(GLFWwindow* window)
 	glBindTexture(GL_TEXTURE_2D, colorBuffers[0]);
 	glActiveTexture(GL_TEXTURE1);
 	glBindTexture(GL_TEXTURE_2D, pingpongColorbuffers[!horizontal]);
-	glUniform1i(glGetUniformLocation(programBloomFinal, "bloom"), bloom);
+	glUniform1i(glGetUniformLocation(programBloomFinal, "bloom"), true);
 	glUniform1f(glGetUniformLocation(programBloomFinal, "exposure"), exposure);
 	renderQuad();
 
@@ -523,7 +504,6 @@ void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 	std::cout << width << height;
 }
 
-// ³aduje tekstury
 TextureSet loadTextureSet(const std::string& albedoPath, const std::string& normalPath, const std::string& aoPath, const std::string& roughnessPath, const std::string& metallicPath) {
 	TextureSet textureSet;
 	textureSet.albedo = Core::LoadTexture(albedoPath.c_str());
@@ -534,7 +514,6 @@ TextureSet loadTextureSet(const std::string& albedoPath, const std::string& norm
 	return textureSet;
 }
 
-// funkcja do tekstur
 void initTextures() {
 	textures.sun.albedo = Core::LoadTexture("./textures/sun/sun_albedo.jpg");
 	textures.sun.normal = Core::LoadTexture("./textures/sun/sun_normal.jpg");
@@ -583,7 +562,6 @@ void initTextures() {
 void initBloom() {
 	glGenFramebuffers(1, &hdrFBO);
 	glBindFramebuffer(GL_FRAMEBUFFER, hdrFBO);
-	// create 2 floating point color buffers (1 for normal rendering, other for brightness threshold values)
 	glGenTextures(2, colorBuffers);
 	for (unsigned int i = 0; i < 2; i++)
 	{
@@ -591,21 +569,18 @@ void initBloom() {
 		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA16F, 1920, 1080, 0, GL_RGBA, GL_FLOAT, NULL);
 		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
 		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);  // we clamp to the edge as the blur filter would otherwise sample repeated texture values!
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
 		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
-		// attach texture to framebuffer
 		glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0 + i, GL_TEXTURE_2D, colorBuffers[i], 0);
 	}
-	// create and attach depth buffer (renderbuffer)
 	unsigned int rboDepth;
 	glGenRenderbuffers(1, &rboDepth);
 	glBindRenderbuffer(GL_RENDERBUFFER, rboDepth);
 	glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH_COMPONENT, 1920, 1080);
 	glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_RENDERBUFFER, rboDepth);
-	// tell OpenGL which color attachments we'll use (of this framebuffer) for rendering 
+
 	unsigned int attachments[2] = { GL_COLOR_ATTACHMENT0, GL_COLOR_ATTACHMENT1 };
 	glDrawBuffers(2, attachments);
-	// finally check if framebuffer is complete
 	if (glCheckFramebufferStatus(GL_FRAMEBUFFER) != GL_FRAMEBUFFER_COMPLETE)
 		std::cout << "Framebuffer not complete!" << std::endl;
 	glBindFramebuffer(GL_FRAMEBUFFER, 0);
@@ -622,7 +597,6 @@ void initBloom() {
 		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE); // we clamp to the edge as the blur filter would otherwise sample repeated texture values!
 		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
 		glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, pingpongColorbuffers[i], 0);
-		// also check if framebuffers are complete (no need for depth buffer)
 		if (glCheckFramebufferStatus(GL_FRAMEBUFFER) != GL_FRAMEBUFFER_COMPLETE)
 			std::cout << "Framebuffer not complete!" << std::endl;
 	}
@@ -632,7 +606,6 @@ void initBloom() {
 	glUseProgram(programBloomFinal);
 	glUniform1i(glGetUniformLocation(programBloomFinal, "scene"), 0);
 	glUniform1i(glGetUniformLocation(programBloomFinal, "bloomBlur"), 1);
-
 }
 
 void init(GLFWwindow* window)
@@ -683,19 +656,18 @@ void shutdown(GLFWwindow* window)
 	glDeleteTextures(1, &skyboxTexture);
 }
 
-//obsluga wejscia
 void processInput(GLFWwindow* window)
 {
 	float angleSpeed = 0.05f * deltaTime * 10;
 	float moveSpeed = 0.6f * deltaTime * 60;
-	
-	// Kopiuj aktualn¹ pozycjê statku do nowej zmiennej
+
 	glm::vec3 newSpaceshipPos = spaceshipPos;
 
-	// SprawdŸ kolizjê po dodaniu wartoœci
-
 	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) glfwSetWindowShouldClose(window, true);
-	if (glfwGetKey(window, GLFW_KEY_X) == GLFW_PRESS) hideInstruction = true;
+	if (glfwGetKey(window, GLFW_KEY_X) == GLFW_PRESS) {
+		hideInstruction = true;
+		hideCompleted = true;
+	}
 	if (hideInstruction == true) {
 		if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS) newSpaceshipPos += spaceshipDir * moveSpeed;
 		if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS) newSpaceshipPos -= spaceshipDir * moveSpeed;
@@ -708,7 +680,7 @@ void processInput(GLFWwindow* window)
 			laser.startTime = glfwGetTime();
 		}
 	}
-	// Jeœli nie wykryto kolizji, zaktualizuj pozycjê statku
+
 	if (checkCollision(newSpaceshipPos, spaceshipRadius) == false) spaceshipPos = newSpaceshipPos;
 
 	cameraPos = spaceshipPos - 1.5 * spaceshipDir + glm::vec3(0, 1, 0) * 0.5f;
